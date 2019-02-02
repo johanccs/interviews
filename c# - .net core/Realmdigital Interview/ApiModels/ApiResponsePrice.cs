d38b782e7c4b31bb6ff627c1ef4a12656201a16e
Johan Potgieter <johan.ccs@gmail.com>
2019-02-02 18:41:06 +0200

    Initial commit. Add unit test project + separating the projects into different folders. Move the ApiResponseProduct and ApiResponsePrice classes into the ApiModels folders

---
+++ c# - .net core/Realmdigital Interview/ApiModels/ApiResponsePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Realmdigital_Interview.ApiModels
+{
+    public class ApiResponsePrice
+    {
+        public string SellingPrice { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
